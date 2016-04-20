c132980a84568d45172e2f9f2420aa8dbeef066d
kellim <kelli.blalock2@gmail.com>
2016-04-20 12:46:20 -0700

    feat: delete match table

--- tournament.py
+++ tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,7 +13,13 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+db = connect();
+cursor = db.cursor()
+# query = "TRUNCATE ONLY match RESTART IDENTITY;"
+query = "DELETE FROM match;"
+cursor.execute(query);
+db.commit()
+db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -25,10 +31,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +62,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
