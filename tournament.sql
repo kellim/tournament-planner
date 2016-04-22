92bbc394861e0e8e0478eff1659a018879bf66f9
kellim <kelli.blalock2@gmail.com>
2016-04-21 22:52:41 -0700

    feat: Add wins view

--- tournament.sql
+++ tournament.sql
@@ -12,15 +12,23 @@ DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
 \c tournament
 
--- Create table for players who will play in the tournament
+-- Create table for players who will play in the tournament.
 CREATE TABLE player (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL
 );
 
--- Create table to record winnners and losers of matches in the tournament
+-- Create table to record winnners and losers of matches in the tournament.
 CREATE TABLE match (
   id SERIAL PRIMARY KEY,
   winner INTEGER NOT NULL REFERENCES player(id),
   loser INTEGER NOT NULL REFERENCES player(id)
 );
+
+-- Create view with the player's ID, player's name, and number of wins.
+CREATE VIEW v_wins as
+SELECT player.id, player.name, COALESCE(COUNT(match.winner), 0) AS wins
+FROM player
+LEFT JOIN match on player.id = match.winner
+GROUP BY player.id
+ORDER BY wins DESC;
